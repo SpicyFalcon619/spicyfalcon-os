b4137f88b5f6bd5587ede43b9826f78d811d4621
Ahmad Maruf Hossain <hossainmysterio619@gmail.com>
2026-06-03 16:39:51 +0600

    fix(os): export Windows7Logo to fix runtime crash when opening winver

--- src/components/shared/BootScreen.jsx
+++ src/components/shared/BootScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import useDesktopStore from '../../store/useDesktopStore';
-const Windows7Logo = ({ size = 120, animated = false }) => {
+export const Windows7Logo = ({ size = 120, animated = false }) => {
   return (
     <div style={{
       width: size,
